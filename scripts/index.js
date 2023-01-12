6b124a8be66dd784eb99161e78aa51034ba9500d
Tjips <thomas@scheepers.com>
2023-01-12 14:05:41 +0100

    Nieuwe start

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
 function toggleDark() {
-    
+    const left = document.querySelector(".left-container");
+    const right = document.querySelector(".right-container");
+    const call_to_action = document.querySelector(".call-to-action-content-container");
+    const title = document.querySelector("h2");
+    left.classList.toggle("dark");
+    right.classList.toggle("dark");
+    title.classList.toggle("whiteTitle");
+    call_to_action,classList.toggle("blackTitle")
 }
