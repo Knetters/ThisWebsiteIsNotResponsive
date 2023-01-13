fdd4845a8e0a4887ff117d21f26bbbcd34645a3c
Tjips <thomas@scheepers.com>
2023-01-13 12:30:56 +0100

    ClearInterval function

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,12 @@ function moveOverlay() {
 }
 
 var numbers = document.getElementById("numbers");
-var count = 0;
-setInterval(function() {
+  var count = 0;
+  var intervalId = setInterval(function() {
     numbers.innerHTML = count;
     count++;
     if (count > 100) {
-        clearInterval();
-        moveOverlay();
+      clearInterval(intervalId);
+      moveOverlay();
     }
 }, 2000); // 2000 milliseconds = 2 seconds
