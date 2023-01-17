1a6328c8eb1229d47b9d10db6982824f89030896
Tjips <thomas@scheepers.com>
2023-01-17 20:29:35 +0100

    font

--- scripts/index.js
+++ scripts/index.js
@@ -21,4 +21,19 @@ var numbers = document.getElementById("numbers");
     }
 }, 2000); // 2000 milliseconds = 2 seconds
 
-herstart.addEventListener('click', moveOverlay)
+herstart.addEventListener("click", moveOverlay)
+
+// Buttons
+const action_btn = document.getElementById("action-btn");
+const info_btn = document.getElementById("info-btn");
+
+action_btn.addEventListener("click", redirectAction)
+info_btn.addEventListener("click", redirectInfo)
+
+function redirectAction() {
+  window.location.href = "/#";
+}
+
+function redirectInfo() {
+  window.location.href = "https://www.digitalnatives.nl/diensten/digitale-toegankelijkheid-en-wcag?gclid=Cj0KCQiAq5meBhCyARIsAJrtdr4-Eg5mevtlGmeI2ga4FQBnDx2fYnBSfDZpVeRdaLMTSsmq9Tg8NdkaAmIlEALw_wcB";
+}
